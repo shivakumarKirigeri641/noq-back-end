1372ce1c09c5608702d869fcc186c1d0922e2c35
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-09-14 10:51:28 +0530

    task:fixes done for search & ticket

--- src/routers/bookingRouter.js
+++ src/routers/bookingRouter.js
@@ -35,7 +35,7 @@ bookingRouter.get(
     const pool = await connectDB(); // get the pool instance
     const client = await pool.connect();
     try {
-      const { src, dest, doj } = req.body;
+      const { src, dest } = req.body;
       if (!src) {
         throw new Error("Source is invalid!");
       }
@@ -55,6 +55,22 @@ bookingRouter.get(
       if (0 === result_fromtodetails.rows.length) {
         throw new Error("Source/Destination not valid!");
       }
+      //check if source is really present?
+      const result_source = await client.query(
+        "select code,station_name from stations where code =$1",
+        [src.toUpperCase()]
+      );
+      //check if destination  is really present?
+      const result_dest = await client.query(
+        "select code,station_name from stations where code =$1",
+        [dest.toUpperCase()]
+      );
+      if (0 === result_source.rows.length) {
+        throw new Error("Invalid source!");
+      }
+      if (0 === result_dest.rows.length) {
+        throw new Error("Invalid Destination!");
+      }
       const result = await client.query(
         `select t.train_number, t.train_name, s1.arrival, s1.departure, s1.station_name as from_station, (s2.kilometer - s1.kilometer) as km, round((s2.kilometer - s1.kilometer)*0.3) as base_fare,
 t.station_from as train_source, t.station_to as train_destination,
@@ -63,11 +79,28 @@ trains t on t.train_number = s1.train_number join coaches c on c.train_number = 
 where
 s1.station_code = $1 and 
 s2.station_code=$2 and 
-s1.arrival > (Now() at time zone 'Asia/Kolkata')::time and
+s1.arrival BETWEEN CURRENT_TIME AND CURRENT_TIME + INTERVAL '4 hours' and
 t.train_runs_on_${shortWeekName} = $3 and
 c.gen='Y' and
 s1.station_sequence <s2.station_sequence`,
         [src.toUpperCase(), dest.toUpperCase(), "Y"]
+      );
+      //user
+      const result_user = await client.query(
+        "select u.id as userid, w.id as walletid from walletdata w join users u on u.id = w.fkuser where u.mobile_number = $1",
+        [req.mobile_number] //undiefind?
+      );
+      //insert into searchdata
+      const result_searchquery = await client.query(
+        "insert into searchdata (fkuser, source_code, destination_code, source, destination,dateofjourney) values ($1,$2,$3,$4,$5,$6)",
+        [
+          result_user.rows[0].userid,
+          result_source.rows[0].code,
+          result_dest.rows[0].code,
+          result_source.rows[0].station_name,
+          result_dest.rows[0].station_name,
+          new Date(Date.now()),
+        ]
       );
       if (0 < result.rows.length) {
         return res.json({
@@ -239,7 +272,7 @@ bookingRouter.post(
         data: {
           train_details: {
             train_number,
-            train_name,
+            train_name: result_traindetails.rows[0].train_name,
             source: result_stationnames.rows[0].station_name,
             destination: result_stationnames.rows[1].station_name,
             train_origin: result_traindetails.station_from,
@@ -265,7 +298,7 @@ bookingRouter.post(
               0 === result_bookedticket.rows[0].paytype ? "online" : "wallet",
           },
           comments: [
-            "Ticket valie till mentioned destination only",
+            "Ticket valid till mentioned destination only",
             "Ticket is strictly NOT transferable",
           ],
         },
